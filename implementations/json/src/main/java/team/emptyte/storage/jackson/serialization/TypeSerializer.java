c813823b9e24015a3be488e711a80ccd1dd58880
srvenient <srvenientmail.com>
2026-06-13 22:07:33 -0500

    feat(json): add JSON repository and type serializer abstractions

---
+++ implementations/json/src/main/java/team/emptyte/storage/jackson/serialization/TypeSerializer.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of storage, licensed under the MIT License
+ *
+ * Copyright (c) 2025 Emptyte Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package team.emptyte.storage.jackson.serialization;
+
+import org.jetbrains.annotations.NotNull;
+import team.emptyte.storage.exception.SerializationException;
+import tools.jackson.core.JsonGenerator;
+import tools.jackson.core.JsonParser;
+
+public interface TypeSerializer<T> {
+	@NotNull T deserialize(final @NotNull JsonParser jsonParser);
+
+	void serialize(final @NotNull T type, final @NotNull JsonGenerator generator) throws SerializationException;
+}
