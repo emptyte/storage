85a3ea0c9a9bb9f82aef072907376f6eb4c5bf64
srvenient <srvenientmail.com>
2026-06-15 17:02:22 -0500

    refactor(build): simplify JavaPublishingConventionPlugin and improve readability

--- build-logic/convention/src/main/kotlin/JavaPublishingConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/JavaPublishingConventionPlugin.kt
@@ -1,12 +1,8 @@
 import net.kyori.indra.IndraExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.publish.PublishingExtension
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.repositories
-import org.gradle.plugins.signing.SigningExtension
 
 class JavaPublishingConventionPlugin : Plugin<Project> {
   override fun apply(target: Project) {
@@ -15,51 +11,25 @@ class JavaPublishingConventionPlugin : Plugin<Project> {
 
       extensions.configure<IndraExtension> {
         github("emptyte", "storage") {
-          ci(true)
+          ci(false)
         }
         mitLicense()
-      }
 
-      afterEvaluate {
-        extensions.configure<IndraExtension> {
-          repositories {
-            maven {
-              name = "sonatype"
-              url = uri("https://central.sonatype.com/repository/maven-releases/")
+        configurePublications {
+          pom {
+            name.set("storage")
+            description.set("The groundbreaking library that breathes life into your data and models with consistent elegance. Effortlessly manage storage while implementing the powerful repository design pattern.")
+            url.set("https://github.com/emptyte/storage")
 
-              credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
+            developers {
+              developer {
+                id.set("srvenient")
+                name.set("Nelson Rodriguez Roa")
+                url.set("https://github.com/srvenient")
+                email.set("srvenient@gmail.com")
               }
             }
           }
-
-          configurePublications {
-            pom {
-              name.set("storage")
-              description.set("The groundbreaking library that breathes life into your data and models with consistent elegance. Effortlessly manage storage while implementing the powerful repository design pattern.")
-              url.set("https://github.com/emptyte/storage")
-
-              developers {
-                developer {
-                  id.set("srvenient")
-                  name.set("Nelson Rodriguez Roa")
-                  url.set("https://github.com/srvenient")
-                  email.set("srvenient@gmail.com")
-                }
-              }
-            }
-          }
-        }
-      }
-
-      extensions.configure<SigningExtension> {
-        val signingKey = System.getenv("GPG_SIGNING_KEY")
-        val signingPassword = System.getenv("GPG_SIGNING_PASSWORD")
-
-        if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
-          useInMemoryPgpKeys(signingKey, signingPassword)
-          sign(extensions.getByType<PublishingExtension>().publications)
         }
       }
     }
