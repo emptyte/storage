1027b51167efa8b1f664ceaa336e3fa24ffe9576
srvenient <srvenientmail.com>
2026-06-15 00:31:00 -0500

    feat(ci): add GitHub Actions workflow for publishing to Maven Central

--- build-logic/convention/src/main/kotlin/JavaPublishingConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/JavaPublishingConventionPlugin.kt
@@ -1,8 +1,11 @@
 import net.kyori.indra.IndraExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.plugins.signing.SigningExtension
 
 class JavaPublishingConventionPlugin : Plugin<Project> {
   override fun apply(target: Project) {
@@ -14,6 +17,35 @@ class JavaPublishingConventionPlugin : Plugin<Project> {
           ci(true)
         }
         mitLicense()
+
+        publishAllTo("sonatype", "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+        configurePublications {
+          pom {
+            name.set("storage")
+            description.set("The groundbreaking library that breathes life into your data and models with consistent elegance. Effortlessly manage storage while implementing the powerful repository design pattern.")
+            url.set("https://github.com/emptyte/storage")
+
+            developers {
+              developer {
+                id.set("srvenient")
+                name.set("Nelson Rodriguez Roa")
+                url.set("https://github.com/srvenient")
+                email.set("srvenient@gmail.com")
+              }
+            }
+          }
+        }
+      }
+
+      extensions.configure<SigningExtension> {
+        val signingKey = System.getenv("GPG_SIGNING_KEY")
+        val signingPassword = System.getenv("GPG_SIGNING_PASSWORD")
+
+        if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
+          useInMemoryPgpKeys(signingKey, signingPassword)
+          sign(extensions.getByType<PublishingExtension>().publications)
+        }
       }
     }
   }
